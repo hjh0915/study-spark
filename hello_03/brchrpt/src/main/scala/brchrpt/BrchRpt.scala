473791b8d97930405f912a23837a94a74a8211a9
hjh0915 <623299157@qq.com>
2020-10-04 20:11:11 +0800

    增加一个子项目查询总和

---
+++ hello_03/brchrpt/src/main/scala/brchrpt/BrchRpt.scala
@@ -0,0 +1,37 @@
+package brchrpt
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import conf.{Utils, CassSetting}
+
+object BrchRpt {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .config("spark.cassandra.connection.host", CassSetting.host)
+      .config("spark.cassandra.auth.username", CassSetting.username)
+      .config("spark.cassandra.auth.password", CassSetting.password)
+      .appName("查询cassandra数据")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val df = spark
+        .read
+        .format("org.apache.spark.sql.cassandra")
+        .options(Map( "table" -> "brch_qry_dtl", "keyspace" -> "finance"))
+        .load
+
+    df.createOrReplaceTempView("qry_dtl")
+
+    spark.sql("""
+        select rpt_sum, sum(amt) from qry_dtl 
+        where tran_date="2019-11-27" and dr_cr_flag="2" 
+        group by rpt_sum
+    """).show()
+
+    spark.stop()
+  }
+}
